383b5a472103dd4a0103308a39690abed8d302a8
Oliwia Obiegła <148152881+aaszlii@users.noreply.github.com>
2024-06-04 00:04:53 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <cstdlib> // Dodano do uzycia funkcji exit()
+#include "CuckooHash.hpp"
+#include "Timer.hpp"
+#include "run.hpp"
+
+using namespace std;
+using namespace std::chrono;
+
+int main() {
+    run();
+    return 0;
+}
