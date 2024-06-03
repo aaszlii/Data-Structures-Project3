fb77955b5635a988fd7b5df2311ebb0e953a3b6c
Oliwia Obiegła <148152881+aaszlii@users.noreply.github.com>
2024-06-04 00:06:37 +0200

    Create Timer.hpp

---
+++ Timer.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <chrono>
+
+class Timer {
+private:
+    std::chrono::time_point<std::chrono::steady_clock> start_time;
+    std::chrono::time_point<std::chrono::steady_clock> end_time;
+
+public:
+    void start() {
+        start_time = std::chrono::steady_clock::now();
+    }
+
+    void stop() {
+        end_time = std::chrono::steady_clock::now();
+    }
+
+    double elapsed_milliseconds() const {
+        return std::chrono::duration<double, std::milli>(end_time - start_time).count();
+    }
+};
